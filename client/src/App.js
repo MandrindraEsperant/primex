80c4acd0a73c6632e2f063df1944e024315ee78a
priscamino12 <priscafanambinantsoa12@gmail.com>
2024-10-02 14:28:51 +0300

    commit primo

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
 import "./App.css";
+import "./App.scss";
+import {useContext, useEffect} from 'react';
+
+import MoonIcon from "./assets/icons/moon.svg";
+import SunIcon from "./assets/icons/sun.svg";
+import { ThemeContext } from "./context/ThemeContext";
+import { DARK_THEME, LIGHT_THEME } from "./constants/themeConstants";
+
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import PublicRouter from "./pages/public/PublicRouter";
 import AdminRouter from "./pages/admin/AdminRouter";
@@ -6,6 +14,16 @@ import AuthRouter from "./pages/auth/AuthRouter";
 import AuthGuard from "./_helpers/AuthGuard";
 
 function App() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  // adding dark-mode class if the dark mode is set on to the body tag
+  useEffect(() => {
+    if (theme === DARK_THEME) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [theme]);
   return (
     <Router>
       <Routes>
@@ -21,6 +39,16 @@ function App() {
         
         <Route path="/auth/*" element={<AuthRouter />} />
       </Routes>
+      <button
+          type="button"
+          className="theme-toggle-btn"
+          onClick={toggleTheme}
+        >
+          <img
+            className="theme-icon"
+            src={theme === LIGHT_THEME ? SunIcon : MoonIcon}
+          />
+        </button>
     </Router>
   );
 }
