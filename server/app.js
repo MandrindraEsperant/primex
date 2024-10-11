0d77d4387dff229cdf0880a3ed5671cbe69fa5fc
priscamino12 <priscafanambinantsoa12@gmail.com>
2024-10-11 13:59:21 +0300

    details transport form

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
-});
+}); 
 
 app.use('/employe', employeRoutes);
 app.use('/client', clientRoutes); 
